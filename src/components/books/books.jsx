097bd1ea7089e5fe45e5b8b4552af98cec460db8
dewaleolaoye <adewaleolaoye21@gmail.com>
2021-05-12 20:08:28 +0100

    ui

---
+++ src/components/books/books.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Books = () => {
+  return (
+    <div className='book'>
+      <p className='book__author'>Author: Ανώνυμος</p>
+      <p className='book__title'>Title: Ο Αλέξανδρος ο Μακεδών</p>
+      <p className='book__pyear'>Year: 1529</p>
+      <p className='book__pcountry'>Country: Ιταλία</p>
+      <p className='book__pcity'>City: Βενετία</p>
+      <p className='book__pages'>Pages: 104</p>
+    </div>
+  );
+};
+
+export default Books;
